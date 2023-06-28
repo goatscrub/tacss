22a1351d84d44b8611e5ba6cfa365e9649b7bca0
goatscrub <>
2023-06-28 02:44:05 +0200

    remove local-server add assests/svg files and fonts add tarteaucitron js files cleanup css/tac-custom with unnecessary "tarteaucitronRoot"

---
+++ js/tarteaucitron-dev.js
@@ -0,0 +1,11 @@
+if (window.location.hostname.match(/^.+\.test$/)) {
+    // function p() { tarteaucitron.userInterface.openPanel() }
+    // const a = tarteaucitron.userInterface.openAlert()
+    // const p = tarteaucitron.userInterface.openAlert()
+
+    window.addEventListener("tac.root_available", () => {
+        tarteaucitron.parameters.EXT_narrow && tarteaucitron.userInterface.addClass("tarteaucitronAlertBig", `${tarteaucitron.parameters.EXT_narrow}`)
+        document.querySelector("#tarteaucitronAlertBig")
+        tarteaucitron.userInterface.css("tarteaucitronAlertBig", "display", "block")
+    })
+}
